9f010d6dcf682f9fc9911f63e83c16079d8e9e8c
NIGHTZNERO <61750641+nightznero@users.noreply.github.com>
2024-05-24 05:02:25 +0200

    Create build.user.js

---
+++ build.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Camwhores.tv Private Vids
+// @namespace    http://www.camwhores.tv/*
+// @version      1.0
+// @description  automatically open private videos in a new tab using the URL script by reddit user Bakolas
+// @author       NIGHTZNERO
+// @match        http://www.camwhores.tv/*
+// @grant        none
+// @require      https://greasyfork.org/scripts/31940-waitforkeyelements/code/waitForKeyElements.js?version=209282
+// ==/UserScript==
+ 
+var baseURL = "https://polar-springs-10127.herokuapp.com/process_get?inputBox=";
+ 
+waitForKeyElements(".private", updatePrivateUrls);
+ 
+function updatePrivateUrls(){
+    $(".private").each(function(){
+        var url = $(this).children('a').attr('href');
+        if(url.includes(baseURL)){
+           return true;
+        }
+        var urlEncode = encodeURIComponent($(this).children('a').attr('href'));
+        $(this).children('a').attr("href",baseURL + url).attr('target','_blank');
+    });
+}

1d6f795eed41e91d3f2154c20a0158a7a0352958
NIGHTZNERO <61750641+nightznero@users.noreply.github.com>
2024-05-24 03:28:33 +0000

    Update build.user.js

--- build.user.js
+++ build.user.js
@@ -4,6 +4,9 @@
 // @version      1.0
 // @description  Open private videos in a new tab using the script by reddit user Bakolas
 // @author       NIGHTZNERO
+// @updateURL    https://raw.githubusercontent.com/nightznero/CW-Bypass/main/build.user.js
+// @downloadURL  https://raw.githubusercontent.com/nightznero/CW-Bypass/main/build.user.js
+// @icon         https://i.ibb.co/5MjjX51/cw-logo.png
 // @match        http://www.camwhores.tv/*
 // @grant        none
 // @require      https://greasyfork.org/scripts/31940-waitforkeyelements/code/waitForKeyElements.js?version=209282
